934a96ede1c142bedf3baa242d689d939ae177b3
moonrise11 <moonrise11@gmail.com>
2014-07-22 15:15:50 +0200

    Improved Holiday, rspec tests

--- spec/models/holiday_spec.rb
+++ spec/models/holiday_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Holiday, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should belong to user' do
+    t = Holiday.reflect_on_association(:user)
+    expect(t.macro).to equal :belongs_to
+  end
+
+  it 'should have attributes :startdate, :enddate, :description, :status, :reason' do
+    expect(subject.attributes).to include('startdate', 'enddate', 'description', 'status', 'reason')
+  end
+
+  it 'must have startdate' do
+    subject.should have(1).error_on(:startdate)
+  end
 end
