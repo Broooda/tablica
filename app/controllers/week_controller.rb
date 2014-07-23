8d237b115c64d418e953ce0440da2229a1bb1627
Vakrim <daffy92@gmail.com>
2014-07-23 15:09:12 +0200

    paski w kalendarzu

--- app/controllers/week_controller.rb
+++ app/controllers/week_controller.rb
@@ -4,6 +4,7 @@ class WeekController < ApplicationController
 
   def showtime #wyswietla kalendarz
     selectweek
+    gethoursplan
 
     @start_hour = 9
     @end_hour = 22
@@ -11,8 +12,17 @@ class WeekController < ApplicationController
 
   def showpeople
     selectweek
-    
+    gethoursplan
+
     @users = User.all
+  end
+
+  def gethoursplan
+    get_from = DateTime.commercial(@year, @week_num, 1) #od poniedzialek polnoc 
+    get_to = DateTime.commercial(@year, @week_num, 5) #do piatek polnoc
+
+    @hours_plans = HoursPlan.where("start_date > :from and start_date < :to", { from: get_from, to: get_to})
+
   end
 
   def selectweek
