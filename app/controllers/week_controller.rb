8bb44769d87a39cc460ff25a094dad5d9fd55bf0
Vakrim <daffy92@gmail.com>
2014-07-23 13:43:46 +0200

    table

--- app/controllers/week_controller.rb
+++ app/controllers/week_controller.rb
@@ -3,12 +3,20 @@ class WeekController < ApplicationController
   layout 'nocontainer' 
 
   def showtime #wyswietla kalendarz
+    selectweek
+
     @start_hour = 9
     @end_hour = 22
   end
 
   def showpeople
+    selectweek
+    
     @users = User.all
   end
 
+  def selectweek
+    @year = Date.today.year
+    @week_num = Date.today.cweek
+  end
 end
