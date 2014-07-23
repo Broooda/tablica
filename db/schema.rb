1bae642e69371e2928d13990a441d79761b9f8c0
adasko90 <adasko90@gmail.com>
2014-07-23 11:07:05 +0200

    Fixed User Model tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140723082156) do
+ActiveRecord::Schema.define(version: 20140723083429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20140723082156) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "surname"
-    t.integer  "defaultWorkTime_id"
     t.boolean  "admin"
   end
 
