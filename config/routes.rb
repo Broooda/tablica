6619c20f7ec27a25f53bbdaee87a4df422649670
moonrise11 <moonrise11@gmail.com>
2014-07-23 12:40:28 +0200

    UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   end
 
   get 'week' => 'week#show' 
+  get 'profile' => 'users#show'
+
+  resource :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
