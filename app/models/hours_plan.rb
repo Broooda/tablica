a5ba931c819bcf909acb9d1dfd8faf7e1b4c48d2
Przemyslaw Szwajkowski <przemyslaw8szwajkowski@gmail.com>
2014-07-24 12:13:58 +0200

    Add new test and validate to HoursPlan

--- app/models/hours_plan.rb
+++ app/models/hours_plan.rb
@@ -1,5 +1,17 @@
 class HoursPlan < ActiveRecord::Base
 	belongs_to :user
-	validate :start_date ,:end_date 
-	# ,presence: true
+	validate :start_date ,:end_date ,presence: true
+	validate :start_date_must_be_before_end_date
+	validate :date_must_be_the_same_end_date
+
+ 	def start_date_must_be_before_end_date
+    	errors.add(:start_date, "must be before end date") unless
+       start_date > end_date
+ 	end
+
+ 	def date_must_be_the_same_end_date
+    	errors.add(:start_date, "must be the same" ,:end_date) unless
+       start_date.strftime("%Y,%m,%d") == end_date.start_date.strftime("%Y,%m,%d")  
+	end
+
 end
--- spec/models/hours_plan_spec.rb
+++ spec/models/hours_plan_spec.rb
@@ -5,8 +5,24 @@ RSpec.describe HoursPlan, :type => :model do
     t = HoursPlan.reflect_on_association(:user)
     expect(t.macro).to equal :belongs_to
  	end
- 	it 'should validate StartDate ,EndDate presence' do
-    # expect(HoursPlan.new).not_to be_valid
-    expect(HoursPlan.new(start_date:DateTime.now,end_date:DateTime.now)).to be_valid
+ end
+
+RSpec::Matchers.define :be_the_same_time_as do |expected|
+  match do |actual|
+    expect(expected.strftime("%Y,%m,%d")).to eq(actual.strftime("%Y,%m,%d"))
+  end
+end
+ 
+describe "time comparison" do
+  it "passes for equal dates" do
+    expect(Time.new(2014, 4, 2)).to be_the_same_time_as Time.new(2014, 4, 2)
+  end
+ 
+  it "passes for equal time" do
+    expect(Time.now).to be_the_same_time_as Time.now
+  end
+ 
+  it "doesn't pass for different time" do
+    expect(Time.now).to_not be_the_same_time_as Time.new(2014, 4, 2)
   end
 end
