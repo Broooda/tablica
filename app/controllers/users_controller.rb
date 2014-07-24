5876a794e3afaea3f9f2a6f8af1f79c08efbb6a3
adasko90 <adasko90@gmail.com>
2014-07-24 11:16:36 +0200

    Added accept metod to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+
+	def accept
+		@user = User.find(params[:id])
+		@user.accept = true
+		@user.save
+		redirect_to users_url
+	end
   
   	def show
     	@user=User.find(params[:id])
