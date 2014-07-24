b82bc10a2e465d6fbfa4da17337491ad05fe927e
adasko90 <adasko90@gmail.com>
2014-07-24 10:39:23 +0200

    Added some user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!, except: [:go_to_login]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:sign_up) << :surname
+  end
+
+  def go_to_login
+  	redirect_to new_user_session_path, notice: 'You must log in.'
   end
 end
