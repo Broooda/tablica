4b3c109ffb6dd4ae410109f8b974a17aa32b0d7c
Przemyslaw Szwajkowski <przemyslaw8szwajkowski@gmail.com>
2014-07-29 12:36:33 +0200

    Repair Holiday model add and setting on validation that the form was not empty

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -3,12 +3,17 @@ class Holiday < ActiveRecord::Base
   validates :startdate, :enddate, :description, :user_id,  presence: true
   validates :status, :inclusion => { :in => %w(accepted rejected pending), message: "It should be accepted, rejected or pending" }
   validates :description, :reason, length: { maximum: 500 } 
-  validate :start_date_must_be_before_end_date ,:presence => { :message => "start date must be erier before end date" }
+  validate :start_date_must_be_before_end_date 
+
 
 
 private
+
 		def start_date_must_be_before_end_date
-    	errors.add(:startdate, "must be before end date") unless 
- 		self.startdate < self.enddate
+			if self.startdate.blank? == true or self.enddate.blank? == true or description.blank? == true
+			else
+    		errors.add(:startdate, "must be earlier before end date") unless 
+ 			self.startdate < self.enddate
+ 		end
  		end
 end
