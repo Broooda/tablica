94cfde3571623a74867af4935ebaadecff2b92a9
Maciej Brodecki <groarq@gmail.com>
2014-07-29 13:50:54 +0200

    check

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -3,17 +3,17 @@ class Holiday < ActiveRecord::Base
   validates :startdate, :enddate, :description, :user_id,  presence: true
   validates :status, :inclusion => { :in => %w(accepted rejected pending), message: "It should be accepted, rejected or pending" }
   validates :description, :reason, length: { maximum: 500 } 
-  validate :start_date_must_be_before_end_date 
+  validates_with :start_date_must_be_before_end_date 
 
 
 
-private
-
+    private
 		def start_date_must_be_before_end_date
-			if self.startdate.blank? == true or self.enddate.blank? == true or description.blank? == true
-			else
-    		errors.add(:startdate, "must be earlier before end date") unless 
- 			self.startdate < self.enddate
- 		end
+			unless self.startdate.blank? == true or self.enddate.blank? == true or description.blank? == true
+    		unless self.startdate < self.enddate
+        errors.add(:startdate, "must be earlier before end date")  
+ 			  end
+ 		   end
  		end
 end
+
