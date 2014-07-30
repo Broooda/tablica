55ca0400d06dcd73b4f6ed432e8947b945cb08c0
moonrise11 <moonrise11@gmail.com>
2014-07-30 14:26:57 +0200

    working on raports

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,8 @@ Rails.application.routes.draw do
 
   get 'layouts/pdfgen' =>'users#index', as: "generate_pdf"
 
+  resources :raports
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
