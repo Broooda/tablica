b978d96d1ea5cafa8c9ed181649e4d5779a21c0b
adasko90 <adasko90@gmail.com>
2014-07-30 12:06:57 +0200

    Added route to raports index

--- app/controllers/raport_controller.rb
+++ app/controllers/raport_controller.rb
@@ -9,7 +9,7 @@ private
 def find_by_day
 	hours_in_that_day.to_a = HoursPlan.where(
 	'user_id = ? and start_date > ? and start_date < ?',
-	default_work_time.user_id,@start_date.to_date,@end_date.to_date
+	default_work_time.user_id, @start_date.to_date, @end_date.to_date
 	)
 end
 
