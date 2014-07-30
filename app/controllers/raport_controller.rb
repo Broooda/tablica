09c6d046f35a3177b14b13f7c4e8f25dffaf50d9
Przemyslaw Szwajkowski <przemyslaw8szwajkowski@gmail.com>
2014-07-30 12:17:22 +0200

    controler raport add date

--- app/controllers/raport_controller.rb
+++ app/controllers/raport_controller.rb
@@ -1,18 +1,15 @@
 class RaportController < ApplicationController
 	def index
-
 	end
 
+def take_day
+
 end
 
-private
-def find_by_day
-	hours_in_that_day.to_a = HoursPlan.where(
-	'user_id = ? and start_date > ? and start_date < ?',
-	default_work_time.user_id,@start_date.to_date,@end_date.to_date
-	)
-end
+	def count_day_of_work
+		TimeDifference.between(start_time, end_time).in_general
+	end
 
-def count_work_hours
+ 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730082151) do
+ActiveRecord::Schema.define(version: 20140728114622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,12 +38,6 @@ ActiveRecord::Schema.define(version: 20140730082151) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-  end
-
-  create_table "holidays_plans", force: true do |t|
-    t.integer  "user_id"
-    t.datetime "hours"
-    t.datetime "holiday_date"
   end
 
   create_table "hours_plans", force: true do |t|
