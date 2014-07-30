66bbd523a0085784f635c48b71a2885d6ea526ff
adasko90 <adasko90@gmail.com>
2014-07-30 12:24:29 +0200

    Conflicts

--- app/controllers/raport_controller.rb
+++ app/controllers/raport_controller.rb
@@ -1,1 +1,18 @@
+class RaportController < ApplicationController
+  def index
+  end
 
+def take_day
+  take_day = HoursPlan.where(
+  'user_id = ? and start_date > ? and start_date < ?',
+  currentuser_id,zmienna_ktora_ma_poczatek_daty,  zmienna_ktora_ma_koniec_daty
+  )
+end
+
+  def count_day_of_work
+    TimeDifference.between(start_time, end_time).in_general
+  end
+
+ 
+
+end
