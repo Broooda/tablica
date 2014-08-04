384129b865e03f077990cd5ef6fc781f8ab62d25
Maciej Brodecki <groarq@gmail.com>
2014-08-04 14:48:30 +0200

    index test

--- spec/controllers/inboxs_controller_spec.rb
+++ spec/controllers/inboxs_controller_spec.rb
@@ -17,6 +17,16 @@ RSpec.describe InboxsController, type: :controller do
         expect(response).to have_http_status(200)
       end
 
+      it 'should have display requests with pending ' do
+        d1=DefaultWorkTimeRequest.create(week: [['9:00','16:00'],['9:00','16:00'],['9:00','16:00'],['9:00','16:00'],['9:00','16:00']], user_id: 2, status: "pending", description: "tralallalaal")
+        d2=DefaultWorkTimeRequest.create(week: [['9:00','16:00'],['9:00','16:00'],['9:00','16:00'],['9:00','16:00'],['9:00','16:00']], user_id: 1, status: "pending", description: "tralallalaal")
+        h1=Holiday.create(startdate: DateTime.now, enddate: DateTime.now+10.days, description: "Urlop", status: "pending", user_id: 2)
+        h2=Holiday.create(startdate: DateTime.now, enddate: DateTime.now+10.days, description: "Urlop", status: "pending", user_id: 1)
+        get :index
+        expect(assigns(:dr)).to match_array([d1,d2])
+        expect(assigns(:holiday)).to match_array([h1,h2])
+
+      end
    end
 
    describe "Get def manual_generate" do
