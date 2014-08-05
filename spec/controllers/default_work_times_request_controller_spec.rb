4a4c64ebd797c78bb1463049f98237ed9fd3935f
moonrise11 <moonrise11@gmail.com>
2014-08-05 13:26:20 +0200

    working rspec

--- spec/controllers/default_work_times_request_controller_spec.rb
+++ spec/controllers/default_work_times_request_controller_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe DefaultWorkTimesRequestController, :type => :controller do
 
   describe "DELETE #destroy" do
-    DefaultWorkTimeRequest.create(user_id: 1, description: "description", status: "pending")
+    DefaultWorkTimeRequest.create(user_id: 1.to_param, description: "description", status: "pending")
 
     it 'should destroy object' do
-      delete :destroy, :id => 1
+      DefaultWorkTimesRequest.any_instance.should_receive(:delete!)
+      delete :destroy, :id => 1.to_param, format: 'js'
     end
   end 
 end
