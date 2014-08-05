29622b486b15f5b638796dbda6234734d5300a83
Vakrim <daffy92@gmail.com>
2014-08-05 12:34:30 +0200

    scrum meetings

--- app/controllers/scrum_meetings_controller.rb
+++ app/controllers/scrum_meetings_controller.rb
@@ -6,9 +6,9 @@ class ScrumMeetingsController < ApplicationController
 
   def show
     @meetings = []
+    @active_user_number = User.active.size
 
     start_date = DateTime.strptime(params[:start_date], '%Y-%m-%d')
-    count = params[:count].to_i
     case params[:period]
     when 'day'
       period = 1
@@ -19,11 +19,16 @@ class ScrumMeetingsController < ApplicationController
     end
 
     counter = 0
-    while counter < count
+    while counter < 5
       from = start_date + counter*period
       to =   start_date + (counter+1)*period
       meeting = HoursPlan.most_people_at_the_same_time(from, to)
       meeting[:start_date] = from
+      holidays_at_this_time = Holiday.accepted.where("startdate <= :time and enddate >= :time", {time: meeting[:time]})
+      meeting[:people_at_holidays] = []
+      holidays_at_this_time.each do |holiday|
+        meeting[:people_at_holidays] << holiday.user
+      end
       @meetings << meeting
       
       counter += 1
