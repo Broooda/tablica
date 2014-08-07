0d9de83d354bdce72213a7c2ec195b570e0611ea
moonrise11 <moonrise11@gmail.com>
2014-08-07 10:41:06 +0200

    problem

--- spec/controllers/default_work_times_request_controller_spec.rb
+++ spec/controllers/default_work_times_request_controller_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe DefaultWorkTimesRequestController, :type => :controller do
 
   describe "DELETE #destroy" do
+    before(:each) do
+      @user=User.create(name: "Jan", surname: "Kowalski", email: "test6463@mail.pl", password: "razdwatrzycztery", accepted: true, admin: true)
+      sign_in @user
+    end
+
     DefaultWorkTimeRequest.create(user_id: 1.to_param, description: "description", status: "pending")
 
     it 'should destroy object' do
