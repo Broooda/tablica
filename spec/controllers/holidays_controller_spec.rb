e9e12a01e34f8c708053d17b42015bbcc0fab0e4
Maciej Brodecki <groarq@gmail.com>
2014-08-07 12:37:11 +0200

    Holidays create/update test

--- spec/controllers/holidays_controller_spec.rb
+++ spec/controllers/holidays_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe HolidaysController, type: :controller do
 
   before(:each) do
     @user=User.create(name: "Jan", surname: "Kowalski", email: "test@mail.pl", password: "razdwatrzycztery", accepted: true)
-    @holiday=Holiday.create(startdate: "2014-07-07 23:03:00", enddate: "2014-08-30 03:09:00", description: "assda", status: "pending", reason: nil, created_at: "2014-08-01 09:32:10", updated_at: "2014-08-01 09:32:10", user_id: 2)
+    @holiday=Holiday.create(startdate: "2014-07-07 23:03:00", enddate: "2014-08-30 03:09:00", description: "assda", status: "pending", reason: nil, created_at: "2014-08-01 09:32:10", updated_at: "2014-08-01 09:32:10", user_id: @user.id)
     sign_in @user
    end
 
@@ -25,6 +25,21 @@ RSpec.describe HolidaysController, type: :controller do
       assigns(:holiday).should be_a_new(Holiday)
     end
   end 
+
+  describe "create" do
+      it "should destroy holiday" do
+      post :create, startdate: "2014-07-07", starttime: "10:00", enddate: "2014-08-30", endtime:"12:00", description: "assda"
+      expect(Holiday.all.size).to match(2)
+      post :create, startdate: "2014-07-07", starttime: "10:00", enddate: "2014-02-30", endtime:"12:00", description: "assda"
+      expect(response).to render_template("new")
+    end
+  end 
+  describe "update" do
+    it "shoud update holiday" do
+    post :update, id: @holiday.id, startdate: "2014-07-07", starttime: "10:00", enddate: "2014-08-30", endtime:"12:00", description: "nienieneie"
+    expect(Holiday.last.description).to match("nienieneie")
+  end
+  end
 end
 
 
